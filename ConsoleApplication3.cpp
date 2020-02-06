63585a753b8e524494c24bb05e8431e7548410cd
iBuddda <Gsoul@mail.ru>
2020-02-06 21:24:36 +0500

    результат

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -1,32 +1,191 @@
 ﻿// ConsoleApplication3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a = 0;
+    cout << "Раз два три поехали\n";
+    //cin >> a;
+    int i = 0;
+    float b = 10;
+    int c = 10;
+    while (i <= 32)
+    
+    {
+        cout <<  c << " " <<(b + 0.4) << endl;
+
+        c++;
+        b++;
+        i++;
+    }
+    
+
+
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float a = 0;
+    cout << "Введите курс\n";
+    cin >> a;
+    int i = 0;
+    int b = 1;
+    while (i < 20)
+    {
+        cout << a * b << endl;
+        b++;
+        i++;
+    }
+
+
+
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float a = 1;
+    cout << "Вес указан в фунтах\n";
+    float b = 0.453;
+
+    for (int i = 0; i < 39; i++ )
+    {
+        cout << a * b << endl;
+        a = a + 0.5;
+        
+    }
+
+
+
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a;
+    cout << "Введите число \n";
+    cin >> a;
+    int b = 1;
+    for (int i = 0; i < 9; i++)
+    {
+        cout << a * b << endl;
+        b++;
+
+    }
+
+
+
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Сумма чисел от 0 до 500\n" ;
+    
+    int b = 0;
+    int i = 0;
+      
+    for (i; i <= 500; i++)
+    {
+    b += i;
+
+    }
+    cout << b << endl;
+
+    
+
+
+
+
+}*/
+/*
 #include <iostream>
 using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
     int a = 0;
-    cout << "Введите число\n";
+    int b = 0;
+    int d;
+    cout << "введите числа\n";
+    cin >> a;
+    cin >> b;
+
+    if (a > b)
+    {
+        d = a;
+        a = b;  
+        b = d;
+    }
+
+    int i = a;
+    int c = a;
+
+
+    for (i; i <= b; i++)
+    {
+        cout << c << endl;
+        c++;
+    }
+        
+}*/
+
+
+/* namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float a = 0;
+    float b = 0;
+    float c = 0;
+    
+    float i = 0;
+    float d;
+    cout << "Введите числа\n";
+    cin >> a;
+    cin >> b;
+
+    if (a > b)
+    {
+        d = a;
+        a = b;
+        b = d;
+    }
+
+       
+    for (i = a+1; i < b; i++)
+    {
+        c += i;
+        
+    }
+    float e = b - a - 1;
+    cout << (c / e) << endl;
+
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float a = 0;
+    //float b = 0;
+    cout << "введите числа\n";
     cin >> a;
     int i = 0;
-    while (i < a)
-    {
-        cout << "20 \n";
-        i++;
-    }
-    
+    for (i;i < a;i++)
+    { }
 
-
-}
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+}*/
